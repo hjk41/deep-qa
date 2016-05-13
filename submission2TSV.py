0fbb71b049312f76514017ea032ea49921ebb646
Chuntao Hong <chuntao.hong@gmail.com>
2016-05-13 17:38:16 +0800

    modify submission2TSV

--- submission2TSV.py
+++ submission2TSV.py
@@ -5,36 +5,68 @@ import sys
 import numpy as np
 import pandas as pd
 
-def idlist2sent(idlist, id2word):
-  return ' '.join([id2word[id] for id in idlist])
+def load_tsv(fname):
+  questions, answers, urls, labels = [], [], [], []
+  for line in open(fname):
+    # Query   Url     PassageID       Passage Rating1 Rating2
+    qupprr = line.split('\t')
+    questions.append(qupprr[0])
+    urls.append(qupprr[1])
+    answers.append(qupprr[3])
+    labels.append(qupprr[5])
+  return questions, answers, urls, labels
 
 def main(argv):
-  if (len(argv) != 3):
-    print('usage: submission2TSV inputDir resultDir')
+  if (len(argv) != 4):
+    print('usage: submission2TSV submissionFile originalTsv resultFile')
     exit(1)
 
-  input_dir = argv[1]
-  result_dir = argv[2]
-  vocab = cPickle.load(open(os.path.join(input_dir, 'vocab.pickle')))
-  id2word = {id:word for word, id in vocab.items()}
-  id2word[len(vocab)] = ''
-  id2word = [id2word[i] for i in range(len(id2word))]
-  qids = np.load(os.path.join(input_dir, 'test.qids.npy'))
-  questions = np.load(os.path.join(input_dir, 'test.questions.npy'))
-  answers = np.load(os.path.join(input_dir, 'test.answers.npy'))
-  labels = np.load(os.path.join(input_dir, 'test.labels.npy'))
-  submission = pd.read_csv(os.path.join(result_dir, 'submission.txt'), 
+  submission_file = argv[1]
+  orig_tsv = argv[2]
+  result_file = argv[3]
+
+  # load original tsv file
+  print('loading original tsv')
+  questions, answers, urls, labels = load_tsv(orig_tsv)
+  # load submission file
+  print('loading submission file')
+  submission = pd.read_csv(submission_file, 
                            names=['qid', 'iter', 'docno', 'rank', 'sim', 'run_id'],
                            sep=' ')
-  qtexts = [idlist2sent(idlist, id2word) for idlist in questions]
-  atexts = [idlist2sent(idlist, id2word) for idlist in answers]
-  N = len(submission)
+  assert len(questions) == len(submission)
+
+  # dump result file
+  print('dumping result file to {}'.format(result_file))
+  rfile = open(result_file, 'w')
+  rfile.write('Query\tUrl\tAnswer\n')
+  best_row = 0
+  best_score = 0
+  last_qid = None
+  for i, r in enumerate(submission.iterrows()):
+    qid = r['qid']
+    score = r['sim']
+    if (not last_qid):
+      last_qid = r['qid']
+    if (qid != last_qid):
+      # a new query, so let's dump last
+      rfile.write('{}\t{}\t{}\n'.format(questions[best_row], urls[best_row], answers[best_row]))
+      best_row = i
+      best_score = score
+    else:
+      if (best_score < score):
+        best_score = score
+        best_row = i
+    last_qid = qid
+  rfile.close()
+
+  # dump file for analysis
+  print('dumping file for analysis')
   result = pd.DataFrame(columns=['label', 'score', 'q', 'a'])
   result['label'] = labels
   result['score'] = submission['sim']
-  result['q'] = qtexts
-  result['a'] = atexts
-  result.to_csv(os.path.join(result_dir, 'result.tsv'), sep='\t')
+  result['q'] = questions
+  result['a'] = answers
+  result.to_csv(result_file, sep='\t')  
 
 if __name__ == '__main__':
   main(sys.argv)
